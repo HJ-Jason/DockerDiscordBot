ff61d78d4efc6eda5d9dfa1d991ea3886ddb6e04
HJ-Jason <jason.heldebaume03@gmail.com>
2022-01-20 17:03:26 +0100

    random

--- DiscordBot.py
+++ DiscordBot.py
@@ -1,6 +1,6 @@
 import sys
 import discord
-
+import random
 
 TOKEN = ''
 f = open("TOKEN", "r")
@@ -16,19 +16,31 @@ async def on_ready():
     print('We have successfully loggged in as {0.user}'.format(client))
     sys.stdout.flush()
 
+random_answers =  [
+    "Très clairement !",
+    "C'est évident.",
+    "Beurk... Non !",
+    "Peut-être...",
+    "Je ne pense pas.",
+]
+
 @client.event
 async def on_message(message):
     if message.author == client.user:
         return
 
     if message.content.lower() == 'salut':
-        await message.channel.send(f'Coucouu {message.author.display_name}!')
+        await message.channel.send(f'Coucouu {message.author.display_name} !')
         return
 
     if client.user in message.mentions:
         await message.channel.send("Qui me ping ?!")
         return
 
+    if "?" in message.content:
+        answer = random.choice(random_answers)
+        await message.channel.send(answer)
+        return
 
 
 client.run(TOKEN)
